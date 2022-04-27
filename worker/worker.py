1fa234024aa985ad618b72aef08b6aa6d93e4a2c
Eric1053 <90023056+Eric1053@users.noreply.github.com>
2022-04-27 14:52:52 -0400

    Update worker.py

--- worker/worker.py
+++ worker/worker.py
@@ -53,7 +53,8 @@ import requests
 import time
 import logging
 import mysql.connector
-
+from flask import Flask, Response
+app = Flask(__name__)
 # try:
 #     connection = mysql.connector.connect(host='localhost',database='coinminer',user= ' ', password=' '
 #     )
@@ -75,6 +76,36 @@ def hash_bytes(data):
     hex_hash = r.text
     return hex_hash
 
+# def work_loop(interval=1):
+#     deadline = 0
+#     loops_done = 0
+#     while True:
+#         if time.time() > deadline:
+#             logging.info("{} units of work done, updating hash counter"
+#                      .format(loops_done))
+#             loops_done = 0
+#             deadline = time.time() + interval
+#         work_once()
+#         loops_done += 1
+
+
+# def work_once():
+#     logging.debug("Doing one unit of work")
+#     time.sleep(0.1)
+#     random_bytes = get_bitcoin_bytes()
+#     hex_hash = hash_bytes(random_bytes)
+#     if not hex_hash.startswith('0'):
+#         logging.debug("No coin found")
+#         return
+#     logging.info("Coin found: {}...".format(hex_hash[:8]))
+#     n = 0
+#     print("Hashes done {}".format(n+1))
+    # created  = """INSERT INTO MyPortfolio (CoinType,Quantity) Values("Bitcoin", "{}")""".format(10)
+    # created = redis.hset("wallet", hex_hash, random_bytes)
+    # if not created:
+    #     logging.info("We already had that coin")
+
+@app.route('/raa')
 def work_loop(interval=1):
     deadline = 0
     loops_done = 0
@@ -99,11 +130,6 @@ def work_once():
     logging.info("Coin found: {}...".format(hex_hash[:8]))
     n = 0
     print("Hashes done {}".format(n+1))
-    # created  = """INSERT INTO MyPortfolio (CoinType,Quantity) Values("Bitcoin", "{}")""".format(10)
-    # created = redis.hset("wallet", hex_hash, random_bytes)
-    # if not created:
-    #     logging.info("We already had that coin")
-
 
 if __name__ == "__main__":
     while True:
