ab962c55e9df26550aadde2574c10d2a8744eb1d
Eric1053 <90023056+Eric1053@users.noreply.github.com>
2022-05-03 10:36:19 -0400

    Update launch_network.sh

--- launch_network.sh
+++ launch_network.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf /users/$USER/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+sudo cp /etc/kubernetes/manifests/kube-apiserver.yaml /local/repository/kube-apiserver.yaml.backup
+sudo sed -i '/^    - --service-cluster-ip-range/a \ \ \ \ - --service-node-port-range=30000-50000' /etc/kubernetes/manifests/kube-apiserver.yaml
+sleep 90
+kubectl create namespace jenkins
+kubectl create clusterrolebinding permissive-binding --clusterrole=cluster-admin --user=admin --user=kubelet --group=system:serviceaccounts
+kubectl -n kube-system create sa jenkins
+kubectl create clusterrolebinding jenkins --clusterrole cluster-admin --serviceaccount=jenkins:jenkins
+kubectl create -f /local/repository/jenkins.yaml --namespace jenkins
+kubectl create -f /local/repository/jenkins-service.yaml --namespace jenkins
